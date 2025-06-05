50e88f1ac6e96ce03b61df709faaaf031ed1bda4
Fallen-angel <topfly1112@gmail.com>
2025-06-05 19:56:34 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {Route, Link} from 'react-router-dom';
+import Sub1 from './components/sub1';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <h1>Test</h1>
+      <header>
+        <h1><Link to='/'>Logo</Link></h1>
+        <nav>
+          <ul>
+            <li><Link to='/components/sub1'>page sub1</Link></li>
+            {/* <li><Link to='/components/sub1'>ahah</Link></li>
+            <li><Link to='/components/sub1'>aksdf</Link></li>
+            <li><Link to='/components/sub1'>asdfasdf</Link></li> */}
+          </ul>
+          
+        </nav>
+      </header>
+      <div>
+        <Route path='/' exact={true} component={Home}/>
+        <Route path='/components/sub1' component={Sub1}/>
+        {/* <Route path='/components/sub1' component={Home}/>
+        <Route path='/components/sub1' component={Home}/>
+        <Route path='/components/sub1' component={Home}/> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/sub1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Sub1(){
+  return(
+    <>
+      <h1>wow</h1>
+    </>
+  )
+};
