4765b463a8596c52069904db9e0dd725161f6c93
Fallen-angel <topfly1112@gmail.com>
2025-06-11 18:52:53 +0900

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import Logoimg from '../images/Logo.png';
+import Logoimg2 from '../images/Logo2.png';
+
 import styles from './Header.module.css';
 import { GiHamburgerMenu } from "react-icons/gi";
 const HeaderArea = styled.div`
-  width: 100%;
+  width: 90vw;
   height: 110px;
   display: flex;
+  left: 5vw;
+  margin: 0 auto;
+  margin-top: 20px;
   border-bottom: 1px solid #000;
+  background: rgba( 255, 255, 255, 0.02 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 15px );
+  -webkit-backdrop-filter: blur( 15px );
+  border-radius: 40vw;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
 `
 
 export default function Header(){
   return(
     <HeaderArea>
       <div className={styles.h_main}>
-        <h1 className={styles.logo}><img src={Logoimg} alt=""/></h1>
+        <h1 className={styles.logo}><img src={Logoimg2} alt=""/></h1>
         <div className={styles.h_nav}>
           <div className={styles.h_nav_top}>
             <ul className={styles.h_nav_top_in}>
