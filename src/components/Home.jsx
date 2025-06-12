2e2579cc92dc299d2f088c81a72d44622af161fd
Fallen-angel <topfly1112@gmail.com>
2025-06-12 21:04:58 +0900

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from './Home.module.css';
 import Mainimg from '../images/MainImg.png';
-import Mainimg2 from '../images/MainImg2.png';
+import Mainimg2 from '../images/Make it natural.png';
 
 export default function Home(){
   return(
     <div>
       <div className={styles.main_box}>
-        <p className={styles.main_ttl}>Taste the Difference</p>
-        <p className={styles.main_sub}>
-          Indulge in the true<br/>
-          taste of premium<br/>
-          ice cream,<br/>
-          Häagen-Dazs.
-        </p>
-        {/* <img className={styles.main_img} src={Mainimg} alt=""/> */}
+        <div className={styles.main_txt_box_back}></div>
+        <div className={styles.main_txt_box}></div>
+          <p className={styles.main_ttl}>Taste the Difference</p>
+          <p className={styles.main_sub}>
+            Indulge in the true<br/>
+            taste of premium<br/>
+            ice cream,<br/>
+            Häagen-Dazs.
+          </p>
+
         <img className={styles.main_img} src={Mainimg2} alt=""/>
         <div className={styles.back_txt}>
           <span>This is Häagen-Dazs</span>
